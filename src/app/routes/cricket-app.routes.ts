e32935e16daabb3d8fad713d9512d043aae49da3
rikrivu <30924294+rikrivu@users.noreply.github.com>
2017-09-23 14:16:35 +0530

    First Commit

---
+++ src/app/routes/cricket-app.routes.ts
@@ -0,0 +1,24 @@
+import { RouterModule, Routes,CanActivate,CanDeactivate} from '@angular/router';
+import { CricketersListComponent } from "app/cricketers/cricketers-list.component";
+import { CricketerComponent } from "app/cricketer/cricketer.component";
+import { CricketerDetailComponent } from "app/cricketer-detail/cricketer-detail.component";
+import { PageNotFoundComponent } from "app/page-not-found/page-not-found.component";
+import { AuthGuardComponent } from "./../routes/auth-guard";
+import { UnsavedComponent}  from "./../routes/unsaved";
+import { NextComponent } from "./../next/next.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/cricketer', pathMatch: 'full' },
+  {path:'login', component:NextComponent},
+  { path: 'cricketer', 
+    component: CricketerComponent,
+    canActivate:[AuthGuardComponent],
+    canDeactivate:[UnsavedComponent]
+  },
+    
+  { path: 'cricketersList', component: CricketersListComponent },
+  { path: 'cricketerDetail/:id', component: CricketerDetailComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+export const appRouting = RouterModule.forRoot(routes);
